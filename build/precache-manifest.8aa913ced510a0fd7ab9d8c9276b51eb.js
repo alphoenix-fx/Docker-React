2aadff998148891f2f117b8ff610d4462815052e
alfredmaboa <alfimaboa@gmail.com>
2020-04-03 16:42:20 +0200

    initial commit

---
+++ build/precache-manifest.8aa913ced510a0fd7ab9d8c9276b51eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c95d4d57567008b6123220d45fe4ff06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a2af3adc1b13288b4d2",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "94a2b7b43e99ca67e6f4",
+    "url": "/static/js/2.6da04d33.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6da04d33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a2af3adc1b13288b4d2",
+    "url": "/static/js/main.6e324cec.chunk.js"
+  },
+  {
+    "revision": "b05506d192fa13a4b14c",
+    "url": "/static/js/runtime-main.cb08f1e8.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
